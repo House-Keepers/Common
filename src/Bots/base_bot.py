3c52f33cfc232bb42a4af949cb4d91f2e9878788
noach844 <noach844@gmail.com>
2022-07-25 20:25:10 +0300

    created twitter bot

--- src/Bots/base_bot.py
+++ src/Bots/base_bot.py
@@ -2,19 +2,7 @@ from abc import ABC, abstractmethod
 
 
 class BaseBot(ABC):
-    def __init__(self, api_url: str = None):
-        """
-        Starts by initialize a session url
-        :param api_url: API url for the channel
-        """
-        self.api_url = api_url
-
-    @abstractmethod
-    def login(self, **kwargs):
-        pass
-
-    @abstractmethod
-    def parse_format(self, **kwargs):
+    def __init__(self):
         pass
 
     @abstractmethod

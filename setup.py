d826cf73291d9f45e68010ded5ae758edeb4a131
noach844 <noach844@gmail.com>
2022-07-26 09:40:46 +0300

    setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='HouseKeepersCommon',
+    version='1.0',
+    author='Cristiano Ronaldo',
+    description='Siiiiii',
+    long_description='Siiiiiiiiiiiiiiiiiiiiiiiiiiiiii',
+    python_requires='>=3.7, <4',
+    install_requires=[
+        'facebook-sdk==3.1.0',
+        'phantomjs==1.4.1',
+        'requests==2.28.1',
+        'selenium==4.3.0',
+        'tweepy~=4.10.0'
+    ]
+)
